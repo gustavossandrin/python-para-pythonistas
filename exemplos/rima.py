d803ebc7ad81e150dc277678022d0bba4f8d7754
Pichau <gustavosandrindattein123@gmail.com>
2022-07-07 21:28:33 -0300

    método de ordenação com parâmetro key
    função de alta ordem

---
+++ exemplos/rima.py
@@ -0,0 +1,8 @@
+frutas = 'banana castanha pequi caju umbu caqui amora'.split()
+
+if __name__ == '__main__':
+    def inverso(fruta):
+        return ''.join(reversed(fruta))
+
+
+    print(sorted(frutas, key=inverso))
