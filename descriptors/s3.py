708b37cc8066b4056d61e4d621360a3c1f2d4564
Pichau <gustavosandrindattein123@gmail.com>
2022-07-10 17:04:32 -0300

    solução de ItemPedido com descriptor de nome ruim

---
+++ descriptors/s3.py
@@ -0,0 +1,55 @@
+import pytest
+from pytest import approx
+
+
+class Quantidade:
+    def __init__(self):
+        self._nome = f'_{id(self)}'
+
+    def __get__(self, item, owner):
+        return getattr(item, self._nome)
+
+    def __set__(self, item, valor):
+        if valor <= 0:
+            raise TypeError('quantidade deveria ser positiva')
+        setattr(item, self._nome, valor)
+
+
+class ItemPedido:
+    quantidade = Quantidade()
+    preco = Quantidade()
+
+    def __init__(self, descricao, preco, quantidade):
+        self.descricao = descricao
+        self.preco = preco
+        self.quantidade = quantidade
+
+    def subtotal(self):
+        return self.preco * self.quantidade
+
+
+def test_subtotal():
+    item = ItemPedido('feijão', 3.45, 2)
+    assert 6.90 == approx(item.subtotal())
+
+
+def test_set_quantidade_negativa():
+    item = ItemPedido('feijão', 3.45, 2)
+    with pytest.raises(TypeError):
+        item.quantidade = -2
+
+
+def test_set_quantidade_negativa_no_init():
+    with pytest.raises(TypeError):
+        ItemPedido('feijão', 3.45, -2)
+
+
+def test_set_preco_negativo():
+    item = ItemPedido('feijão', 3.45, 2)
+    with pytest.raises(TypeError):
+        item.preco = -3.45
+
+
+def test_set_preco_negativo_no_init():
+    with pytest.raises(TypeError):
+        ItemPedido('feijão', -3.45, 2)
